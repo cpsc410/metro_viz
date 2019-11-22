cf7ca0f8c99f6ae7edd382bf3ab103df857f95c5
b3x0b <b3x0b@ugrad.cs.ubc.ca>
2019-11-21 20:16:14 -0800

    hacked together some kind of rendering * using transit-map instead of d3-tube-map * we still need to set labels

--- public/metro.js
+++ public/metro.js
@@ -1,2 +1,22 @@
 console.log("metro renderer running...");
-d3.select("body").style("background-color", "black");
+const container = d3.select("#mapHolder");
+
+const width = 1600;
+const height = 1000;
+
+d3.xml('./api/map')
+  .then(data => {
+    container.node().append(data.documentElement);
+    const svg = container.select('svg');
+
+    svg.attr("width", "100%").attr("height", "100%");
+    function zoomed() {
+      svg.select('g').attr('transform', d3.event.transform.toString());
+    }
+    const zoomy = d3
+      .zoom()
+      .scaleExtent([0.5, 6])
+      .on('zoom', zoomed);
+
+    const zoomContainer = svg.call(zoomy);
+  });
