4ca20df749119f410a26edb461abacd159336155
Ben Henaghan <benjamin.henaghan@gmail.com>
2019-11-23 10:34:29 -0800

    WIP version of the layout engine

--- src/Server.ts
+++ src/Server.ts
@@ -2,26 +2,32 @@ import { Request, Response } from 'express';
 import express = require('express');
 import InputSystem from './input/InputSystem';
 import BoringStaticJSONReader from './input/BoringStaticJSONReader';
-import SnapperFacade from './layout/SnapperFacade';
-import { LayoutNode } from './layout/LayoutNode';
+// import SnapperFacade from './layout/SnapperFacade';
+// import { LayoutNode } from './layout/LayoutNode';
+import LayoutEngine from './layout/LayoutEngine';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+
+const input: InputSystem = new BoringStaticJSONReader();
+// console.log(input.fetchData());
+const layout: LayoutEngine = new LayoutEngine()
+layout.layoutNodes(input.fetchData());
+
 // do all the processing here
 
-let sampleFileA: LayoutNode = {name: "A", x: 0, y: 0, edges: []};
-let sampleFileB: LayoutNode = {name: "B", x: 10, y: 0, edges: []};
-let sampleFileC: LayoutNode = {name: "C", x: 20, y: 10, edges: []};
-sampleFileA.edges.push({contributor: "noa", target: sampleFileB});
-sampleFileB.edges.push({contributor: "noa", target: sampleFileC});
+// let sampleFileA: LayoutNode = {name: "A", x: 0, y: 0, edges: []};
+// let sampleFileB: LayoutNode = {name: "B", x: 10, y: 0, edges: []};
+// let sampleFileC: LayoutNode = {name: "C", x: 20, y: 10, edges: []};
+// sampleFileA.edges.push({contributor: "noa", target: sampleFileB});
+// sampleFileB.edges.push({contributor: "noa", target: sampleFileC});
 
 let map = "";
 
-SnapperFacade.SNAP_NODES([sampleFileA, sampleFileB, sampleFileC]).then((r: any) => map = r).catch((err: any) => console.log(err));
+// SnapperFacade.SNAP_NODES([sampleFileA, sampleFileB, sampleFileC]).then((r: any) => map = r).catch((err: any) => console.log(err));
 
-const input: InputSystem = new BoringStaticJSONReader();
-console.log(input.fetchData());
 
 app.get('/api/map', (_req: Request, res: Response) => {
   res.end(map);
--- src/layout/LayoutEngine.ts
+++ src/layout/LayoutEngine.ts
@@ -1,5 +1,6 @@
 import { AnalysisFileList} from '../input/AnalysisOutput';
-import { LayoutNode, NodeEdge } from './LayoutNode'
+import { LayoutNode } from './LayoutNode'
+// import { LayoutNode, NodeEdge } from './LayoutNode'
 
 export default class LayoutEngine {
 
@@ -7,11 +8,12 @@ export default class LayoutEngine {
     private readonly SIZE_X = 1000;
     private readonly SIZE_Y = 600;
 
-    private readonly MIN_DISTANCE = 10;
-    private readonly SNAP_DISTANCE = 3;
+    private readonly MIN_DISTANCE = 5;
+    private readonly INFLUENCE_DISTANCE = 200;
+    private readonly SNAP_DISTANCE = 6;
 
     private readonly MOVE_FACTOR_TOWARDS = 0.4;
-    private readonly MOVE_FACTOR_AWAY = 0.05;
+    private readonly MOVE_FACTOR_AWAY = 0.1;
 
     public layoutNodes(fileList: AnalysisFileList): LayoutNode[] {
         let nodes: LayoutNode[] = [];
@@ -21,14 +23,25 @@ export default class LayoutEngine {
                 name: file.name,
                 contributors: file.contributors,
                 edges: null,
-                x: Math.random() * this.SIZE_X,
-                y: Math.random() * this.SIZE_Y
+                x: Math.random() * (this.SIZE_X * 0.5),
+                y: Math.random() * (this.SIZE_Y *  0.5)
             })
         })
 
-        nodes.forEach((target) => {
-            nodes.forEach((node) => this.pushPull(node, target));
-        });
+        for (let i = 0; i < 1000; i++) {
+            // nodes.forEach((target) => {
+            //     let nIndex = Math.floor(Math.random() * (nodes.length - 1));
+            //     this.pushPull(nodes[nIndex], target)
+            //     // nodes.forEach((node) => this.pushPull(node, target));
+            // });
+            let index2 = Math.floor(Math.random() * (nodes.length - 1));
+            let index1 = Math.floor(Math.random() * (nodes.length - 1));
+            this.pushPull(nodes[index1], nodes[index2])
+        }
+
+        nodes.forEach((node) => {
+            false && this.trySnaptoNearest(node, nodes);
+        })
 
         //Load all files as nodes
         // let packages = new Set(); 
@@ -54,15 +67,26 @@ export default class LayoutEngine {
         // fileList.forEach((f) => {
             
         // });
+        this.printAsCsv(nodes);
 
         return nodes;
+    }
+
+    private printAsCsv(nodes: LayoutNode[]): void {
+        nodes.forEach((n) => {
+            let output = "";
+            output += n.name + ", ";
+            output += n.x + ", ";
+            output += n.y + ", ";
+            console.log(output);
+        });
     }
 
     private pushPull(node: LayoutNode, compare: LayoutNode): void {
         let dist = this.euclidianDistance(node, compare);
         if (dist > this.MIN_DISTANCE) {
-            let diffX = node.x - compare.x;
-            let diffY = node.y - compare.y;
+            let diffX = compare.x - node.x;
+            let diffY = compare.y - node.y;
             if (node.contributors.some(r => compare.contributors.indexOf(r) >= 0)) {
                 // Shares some contribs
                 node.x += diffX * this.MOVE_FACTOR_TOWARDS;
@@ -70,19 +94,44 @@ export default class LayoutEngine {
 
                 //Snap
                 // TODO is this the right place? maybe we should do this at the end
-                if (diffY < this.SNAP_DISTANCE) {
-                    node.x = compare.x
-                } else if (diffY < this.SNAP_DISTANCE) {
-                    node.y = compare.y;
-                }
-            } else {
+            } else if (dist < this.INFLUENCE_DISTANCE) {
                 node.x -= diffX * this.MOVE_FACTOR_AWAY;
                 node.y -= diffY * this.MOVE_FACTOR_AWAY;
+                console.log("move away x = " + String(diffX * this.MOVE_FACTOR_AWAY))
+                console.log("move away y = " + String(diffY * this.MOVE_FACTOR_AWAY))
             }
+            
+            node.x = node.x > this.SIZE_X ? this.SIZE_X : node.x
+            node.x = node.x < 0 ? 0 : node.x
+            node.y = node.y > this.SIZE_Y ? this.SIZE_Y : node.y
+            node.y = node.y < 0 ? 0 : node.y
         }
     }
 
     private euclidianDistance(from: LayoutNode, to: LayoutNode): number {
         return Math.sqrt(Math.pow(Math.abs(from.x - to.x), 2) + Math.pow(Math.abs(from.y - to.y), 2));
+    }
+
+    private trySnaptoNearest(node: LayoutNode, nodes: LayoutNode[]) {
+        //Find closest node (probably not the fastest way of doin this)
+        let minDist = Number.MAX_VALUE;
+        let minNode;
+        nodes.forEach((n)  => {
+            let dist = this.euclidianDistance(node, n);
+            if (dist < minDist && dist > 0) {
+                dist = minDist;
+                minNode = n;
+            }
+        })
+
+        let diffX = node.x - minNode.x;
+        let diffY = node.y - minNode.y;
+
+        if (diffX < this.SNAP_DISTANCE) {
+            node.x = minNode.x
+        } else if (diffY < this.SNAP_DISTANCE) {
+            node.y = minNode.y;
+        }
+
     }
 }
