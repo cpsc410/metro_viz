831c81474fb75d4903883d7e22efcd4080a4c611
b3x0b <b3x0b@ugrad.cs.ubc.ca>
2019-11-23 11:50:49 -0800

    color picker

--- src/layout/SnapperFacade.ts
+++ src/layout/SnapperFacade.ts
@@ -2,10 +2,12 @@ import transitMap = require('transit-map');
 import { LayoutNode } from './LayoutNode';
 import svgTransitMap = require('svg-transit-map');
 import virtualDomStringify = require('virtual-dom-stringify');
+import ColorPicker from './ColorPicker';
 
 
 export default class SnapperFacade {
   public static async SNAP_NODES(nodes: LayoutNode[]): Promise<any>{
+      let color: ColorPicker = new ColorPicker();
       let input: any = {"nodes": [], "lines": [], "edges": []};
       let lineNames: Set<string> = new Set();
       let edgeMap = {};
@@ -23,7 +25,7 @@ export default class SnapperFacade {
         input.edges.push(edge);
       }
       lineNames.forEach((name: string) => {
-        input.lines.push({id: name, color: "#0a3c85"});
+        input.lines.push({id: name, color: color.getNext()});
       });
       let map = await transitMap(input);
       const svg = svgTransitMap(map, false);
