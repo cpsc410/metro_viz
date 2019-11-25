d7215980e619f9c5f67221c057e8fc132f12e419
b3x0b <b3x0b@ugrad.cs.ubc.ca>
2019-11-25 12:12:07 -0800

    flip colors

--- src/layout/ColorPicker.ts
+++ src/layout/ColorPicker.ts
@@ -1,12 +1,22 @@
 export default class ColorPicker {
-  private colors: string[] = ['#a45a2a', "#da291c", "#ffcd00", "#007a33", "#e89cae", "#7c878e", "#840b55", "#000000", "#10069f", "#00a3e0", "#6eceb2"];
+  private colors: string[] = ['#a45a2a',
+    "#da291c",
+    "#ffcd00",
+    "#007a33",
+    "#e89cae",
+    "#7c878e",
+    "#840b55",
+    "#000000",
+    "#10069f",
+    "#00a3e0",
+    "#6eceb2"];
 
 
   constructor() {
   }
 
   public getNext(): string{
-    return this.colors.pop();
+    return this.colors.shift();
   }
 
 }
